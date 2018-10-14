ec4f61cbe538c9f23afc33c404973ca2d6364b52
Anteneh M <amoges1@gmail.com>
2018-10-14 11:43:49 -0400

    remove unused code

--- src/components/Resframe.js
+++ src/components/Resframe.js
@@ -10,7 +10,7 @@ class Resframe extends Component {
         let resload;
         if (res) {
             resload = Object.keys(res).map(
-                key => <Resitems index={key} key={key} res={res[key]} user={this.props.user} removePlace={this.removePlace} />)
+                key => <Resitems index={key} key={key} res={res[key]} user={this.props.user} />)
         }
         return (
             <div className="container" style={{paddingTop: "20px"}} id="restaurantaccordion" role="tablist" aria-multiselectable="true">
