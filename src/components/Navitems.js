55e3ba7149fd299ea55a8793bc6c034a4a6e126d
Anteneh M <amoges1@gmail.com>
2018-12-25 13:54:25 -0500

    update Navitems/ignore files

--- src/components/Navitems.js
+++ src/components/Navitems.js
@@ -2,11 +2,17 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class Navitems extends Component {
-  render() {
-    let username = this.props.name ? true : false;
 
-    if(username) {
-        username = this.props.name.split(" ")[0];
+  render() {
+
+    let navMenu = [
+        "Home",
+        "Friends",
+        "Search"
+    ];
+
+    if(this.props.name) {
+      
         return (
             <nav className="navbar navbar-dark navbar-expand-sm" style={{backgroundColor:'red'}}>
                 <div className="container">
@@ -15,22 +21,25 @@ class Navitems extends Component {
                     <button className="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#myNav" aria-controls="myNav" 
                     aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
-                    </button>
+                    </button> 
                     
                     <div className="collapse navbar-collapse" id="myNav">
                         <div className="navbar-nav ml-auto">
-                            <Link to="/home" className="nav-item nav-link navLink text-white">Home</Link>
-                            <Link to="/friends" className="nav-item nav-link navLink text-white">Friends</Link>
-                            <Link to="/search" className="nav-item nav-link navLink text-white">Search</Link>
+                            {
+                                navMenu.map((nav, index) => {
+                                    return <Link key={`${index}`} to={`/${nav.toLowerCase()}`} 
+                                        className="nav-item nav-link navLink text-white"> {nav} </Link>
+                                })
+                            }
                             <div className="dropdown">
                                 <a className="nav-item nav-link dropdown-toggle text-white" 
                                     id="userDropdown" data-toggle="dropdown"
-                                aria-haspopup="true" aria-expanded="false" href="#user">{username}</a>
+                                aria-haspopup="true" aria-expanded="false" href="#user">{this.props.name.split(" ")[0]}</a>
                                 
                                 <div className="dropdown-menu" aria-labelledby="userDropdown">
-                                    <a className="dropdown-item">Password Reset</a>
-                                    <a className="dropdown-item">Delete Account</a>
-                                    {/* https://firebase.google.com/docs/auth/web/manage-users */}
+                                    {/* <a className="dropdown-item disabled">Password Reset</a>
+                                    <a className="dropdown-item disabled">Delete Account</a> */}
+                                    {/* TODO https://firebase.google.com/docs/auth/web/manage-users */}
                                     <Link to="/" className="dropdown-item" onClick={this.props.logout}>Logout</Link>
                                 </div>    
                             </div>
@@ -39,10 +48,10 @@ class Navitems extends Component {
                 </div>
             </nav>
         );
-    } else {
-        return <div></div>;
+    } 
+    else {
+        return null;
     }
-
   }
 }
 
