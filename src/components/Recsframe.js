e3f73b19f15dca11a64a19f7e32caf9c2d54a371
Anteneh M <amoges1@gmail.com>
2019-01-17 21:28:17 -0500

    add logic, functions from rec.js

--- src/components/Recsframe.js
+++ src/components/Recsframe.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react';
-import Recs from './Recs';
+import Rec from './Rec';
+import base from '../base';
 
 class Recsframe extends Component {
     
+    removeRec = (e, key) => {
+        e.preventDefault();
+        base.remove(`users/${this.props.user_id}/restaurants/${key}`, 
+        function(err) { 
+            if(err) { console.log(err);  }
+        });
+    }
+
+    addRec = (e, key) =>{
+        e.preventDefault();
+        base.update(`users/${this.props.user_id}/restaurants/${key}`, {
+            data: { added : true},
+            then(err){
+                if(err){ console.log(err); }
+            }
+        });
+    }
+
     render() {
-        const recs = this.props.recs;
-        
-        
-        let recsload;
-        if (recs) {
-            recsload = Object.keys(recs).map(key => <Recs index={key} key={key} recs={recs[key]} user={this.props.user} user_id={this.props.user_id}/>)
-        }
+
         return (
             <div className="container border-bottom mt-20" style={{ marginTop: "20px"}} data-aos="fade-right"  data-aos-duration="1000">
                 <h6 className="alert alert-warning alert-heading d-flex justify-content-between align-items-center" data-toggle="collapse" data-target="#recs">
-                    New Recommendations!
-                    <span className="badge badge-warning">{recsload ? recsload.length : 0} </span>
+                    { this.props.recs.length > 0 ? "New Recommendations!" : "No Recommendations"}
+                    <span className="badge badge-warning">{this.props.recs ? this.props.recs.length : 0} </span>
                 </h6>
                 <div id="recs" className="collapse">
                     {
-                        recsload
+                        Object.keys(this.props.recs).map(key => <Rec index={key} key={key} rec={this.props.recs[key]} 
+                            user={this.props.user} addRec={this.addRec} removeRec={this.removeRec}/>)
                     }
                 </div>
             </div>
