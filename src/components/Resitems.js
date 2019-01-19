d75964683b92edbd3751c8a0d258adf0a60b733a
Anteneh M <amoges1@gmail.com>
2019-01-18 22:21:17 -0500

    convert to functional/stateless components

--- src/components/Resitems.js
+++ src/components/Resitems.js
@@ -1,98 +1,57 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Review from './Review';
 
-class Resitems extends Component {
-    constructor() {
-        super();
-        this.getShareInfo = this.getShareInfo.bind(this);
-        this.getDeleteInfo = this.getDeleteInfo.bind(this);
-        this.state = {
-            res: {}
-        }
-    }
+const Resitems = ({place, id, getShareInfo, getDeleteInfo}) => {
+    
+    const mapAddress = `https://www.google.com/maps/search/${place.name}+${place.address}`
 
-    getShareInfo(e, key) {
-        e.preventDefault();
+    return (
         
-        let shareRes = this.props.user.restaurants[key];
-       
-        document.getElementById("res_name").innerHTML = shareRes.name;
-        document.getElementById("res_address").innerHTML = shareRes.address;
-        document.getElementById("res_rating").innerHTML = shareRes.rating;
-        
-    }
+        <div className="card" style={{marginBottom: "10px"}} data-aos="fade-up"  data-aos-duration="1000">
+            <div className="card-header" style={{ backgroundColor: "red"}} id="restaurantheading" data-toggle="collapse" data-parent="restaurantaccordion"
+            data-target={`#${id}`} aria-expanded="true" aria-controls="restaurantID">
+                <h5 className="mb-0 d-flex justify-content-between" style={{color: "white"}}>
+                    {place.name}
+                    <span className="badge badge-success">{place.rating}</span>
+                </h5>
+            </div> 
 
-    getDeleteInfo(e, key) {
-        e.preventDefault();
-        let shareRes = this.props.user.restaurants[key];
-
-        document.getElementById("dres_name").innerHTML = shareRes.name;
-        document.getElementById("dres_address").innerHTML = shareRes.address;
-        document.getElementById("dres_rating").innerHTML = shareRes.rating;
-        document.getElementById("deletebutton").setAttribute("data-key", key);
-    }
-    render() {
-        const res  = this.props.res;
-        
-        let key = Object.keys(this.props.user.restaurants)
-            .find(key => this.props.user.restaurants[key] === res);
-        
-        //https://www.google.com/maps/search/Shake+Shack+200+Broadway+New+York+NY+10007
-        let address = `https://www.google.com/maps/search/${res.name}+${res.address.split("+")}`
-        let resload;
-        if (res.reviews) {
-            resload = Object.keys(res['reviews']).map( key=>
-                <Review key={key} review={res['reviews'][key]}/>
-            )
-        } 
-
-        return (
-            
-            <div className="card" style={{marginBottom: "10px"}} data-aos="fade-up"  data-aos-duration="1000">
-                <div className="card-header" style={{ backgroundColor: "red"}} id="restaurantheading" data-toggle="collapse" data-parent="restaurantaccordion"
-                data-target={`#${this.props.index}`} aria-expanded="true" aria-controls="restaurantID">
-                    <h5 className="mb-0 d-flex justify-content-between" style={{color: "white"}}>
-                        {res.name}
-                        <span className="badge badge-success">{res.rating}</span>
-                    </h5>
-                </div> 
-
-                <div id={`${this.props.index}`} className="collapse show" role="tabpanel"
-                aria-labelledby="restaurantheading">
-                    <div className="card-block container" >
-                        <div className="mt-3 mb-0">
-                                <h5 className="lead">  {res.address}</h5>
+            <div id={`${id}`} className="collapse show" role="tabpanel" aria-labelledby="restaurantheading">
+                <div className="card-block container" >
+                    <div className="mt-3 mb-0">
+                            <h5 className="lead">  {place.address}</h5>
+                    </div>
+                    <div className="d-flex btn-group" style={{paddingTop: "20px"}}>
+                        <button onClick={ (e) => getShareInfo(e, id)} type="button" className="btn btn-success flex-fill" data-toggle="modal" data-target="#share">
+                            Share <i className="fas fa-user-friends"></i>
+                        </button>
+                        <a href={mapAddress} target="_blank" rel="noopener noreferrer" className="btn btn-warning flex-fill">
+                            Location <i className="fas fa-map-marker-alt"></i>
+                        </a> 
+                        <button data-key={id} onClick={ (e) => getDeleteInfo(e, id)} type="button" className="btn btn-danger flex-fill" data-toggle="modal" data-target="#delete">
+                            Delete <i className="fas fa-trash-alt"></i></button>
+                    </div> 
+                    <div className="row" style={{paddingTop: "20px"}}>
+                        <div className="container">
+                            <table className="table table-striped ">
+                                <tbody>
+                                    {
+                                        place.reviews ? (
+                                            Object.keys(place['reviews']).map( key=>
+                                                <Review key={key} review={place['reviews'][key]}/>)
+                                        ) : (     
+                                            null
+                                        )
+                                    }
+                                </tbody>
+                            </table>
                         </div>
-                        <div className="d-flex btn-group" style={{paddingTop: "20px"}}>
-                          
-                            <button onClick={ (e) => this.getShareInfo(e, key)} type="button" className="btn btn-success flex-fill" data-toggle="modal" data-target="#share">
-                                Share <i className="fas fa-user-friends"></i>
-                            </button>
-                            <a href={address} target="_blank" rel="noopener noreferrer" className="btn btn-warning flex-fill">
-                                Location <i className="fas fa-map-marker-alt"></i>
-                            </a> 
-                            <button data-key={this.props.index} onClick={ (e) => this.getDeleteInfo(e, key)} type="button" className="btn btn-danger flex-fill" data-toggle="modal" data-target="#delete">
-                                Delete <i className="fas fa-trash-alt"></i></button>
-                        </div> 
-                        <div className="row" style={{paddingTop: "20px"}}>
-                            <div className="container">
-                                <table className="table table-striped ">
-                                    <tbody>
-                                        {
-                                            
-                                            resload
-                                        
-                                        }
-                                        
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div> 
                     </div> 
                 </div> 
             </div> 
-        );
-    }
+        </div> 
+    );
+    
 }
 
 export default Resitems;
--- src/components/Review.js
+++ src/components/Review.js
@@ -1,29 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-
-
-class Review extends Component {
-  
-    render() {
-        const review = this.props.review;
-        
-        return (
-            <tr>
-                <td>
-                <blockquote className="blockquote text-center">
-                    <p className="mb-0">{ review.comment } </p>
-                    <footer className="blockquote-footer">{review.name} </footer>
-                </blockquote>
-                </td>
-                <td style={{textAlign: "center"}}>
-                    <span className="badge badge-warning">
-                        { review.score} 
-                    </span> 
-                </td>
-            </tr>
-        )
-        
-    }
+const Review = ({review}) => {  
+    return (
+        <tr>
+            <td>
+            <blockquote className="blockquote text-center">
+                <p className="mb-0">{ review.comment } </p>
+                <footer className="blockquote-footer">{review.name} </footer>
+            </blockquote>
+            </td>
+            <td style={{textAlign: "center"}}>
+                <span className="badge badge-warning">
+                    { review.score} 
+                </span> 
+            </td>
+        </tr>
+    )
 }
+
 
 export default Review;
