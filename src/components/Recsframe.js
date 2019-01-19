6c366243304b315119f2084a14b1fb76173ec7bc
Anteneh M <amoges1@gmail.com>
2019-01-19 12:17:18 -0500

    converted to functional component

--- src/components/Recsframe.js
+++ src/components/Recsframe.js
@@ -2,43 +2,46 @@ import React, { Component } from 'react';
 import Rec from './Rec';
 import base from '../base';
 
-class Recsframe extends Component {
+
+const removeRec = (e, key, user_id) => {
+    e.preventDefault();
+    base.remove(`users/${user_id}/restaurants/${key}`, 
+    function(err) { 
+        if(err) { console.log(err);  }
+    });
+}
+
+const addRec = (e, key, user_id) =>{
+    e.preventDefault();
+    base.update(`users/${user_id}/restaurants/${key}`, {
+        data: { added : true},
+        then(err){
+            if(err){ console.log(err); }
+        }
+    });
+}
+
+const Recsframe = ({recs, user, user_id}) => {
     
-    removeRec = (e, key) => {
-        e.preventDefault();
-        base.remove(`users/${this.props.user_id}/restaurants/${key}`, 
-        function(err) { 
-            if(err) { console.log(err);  }
-        });
-    }
-
-    addRec = (e, key) =>{
-        e.preventDefault();
-        base.update(`users/${this.props.user_id}/restaurants/${key}`, {
-            data: { added : true},
-            then(err){
-                if(err){ console.log(err); }
+    return (
+        <div className="container mt-20" style={{ marginTop: "20px"}} data-aos="fade-right"  data-aos-duration="1000">
+            {
+                recs.length > 0 ? (
+                    <div>
+                        <h6 className="alert alert-warning alert-heading d-flex justify-content-between align-items-center" data-toggle="collapse" data-target="#recs">
+                            New Recommendations! <span className="badge badge-warning">{recs.length} </span>
+                        </h6>
+                        <div id="recs" className="collapse">
+                            {
+                                Object.keys(recs).map(key => <Rec index={key} key={key} rec={recs[key]} 
+                                user={user} user_id={user_id} addRec={addRec} removeRec={removeRec}/>)
+                            }
+                        </div>
+                    </div>
+                ) : null
             }
-        });
-    }
-
-    render() {
-
-        return (
-            <div className="container border-bottom mt-20" style={{ marginTop: "20px"}} data-aos="fade-right"  data-aos-duration="1000">
-                <h6 className="alert alert-warning alert-heading d-flex justify-content-between align-items-center" data-toggle="collapse" data-target="#recs">
-                    { this.props.recs.length > 0 ? "New Recommendations!" : "No Recommendations"}
-                    <span className="badge badge-warning">{this.props.recs ? this.props.recs.length : 0} </span>
-                </h6>
-                <div id="recs" className="collapse">
-                    {
-                        Object.keys(this.props.recs).map(key => <Rec index={key} key={key} rec={this.props.recs[key]} 
-                            user={this.props.user} addRec={this.addRec} removeRec={this.removeRec}/>)
-                    }
-                </div>
-            </div>
-        )
-    }
-      
+            
+        </div>
+    )  
 }
 export default Recsframe;
