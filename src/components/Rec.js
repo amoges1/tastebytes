09afdb30ff16c01c581def406a613f5a6e495627
Anteneh M <amoges1@gmail.com>
2019-01-19 12:18:07 -0500

    adjusted add/removeRec paramaters with Recsframe

--- src/components/Rec.js
+++ src/components/Rec.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 
-const Rec = ({user, rec, addRec, removeRec}) => {
+const Rec = ({user, rec, addRec, removeRec, user_id}) => {
  
     //Find rec's key/ID within user's restaurant
     const key = Object.keys(user.restaurants)
@@ -18,12 +18,12 @@ const Rec = ({user, rec, addRec, removeRec}) => {
             <div className="card-footer d-flex justify-content-center">
                 <div className="btn-group d-flex">
                     <button type="button" className="btn btn-sm btn-success" 
-                        onClick={ (e) => addRec(e, key)}>Add <i className="fas fa-plus-circle"></i>
+                        onClick={ (e) => addRec(e, key, user_id)}>Add <i className="fas fa-plus-circle"></i>
                     </button>
                     <button type="button" className="btn btn-sm btn-warning" data-toggle="modal" 
                         data-target="#map">Location <i className="fas fa-map-marker-alt"></i>
                     </button>
-                    <button type="button" className="btn btn-sm btn-danger" onClick={ (e) => removeRec(e, key)}>
+                    <button type="button" className="btn btn-sm btn-danger" onClick={ (e) => removeRec(e, key, user_id)}>
                         Delete <i className="fas fa-trash-alt"></i>
                     </button>
                 </div>
