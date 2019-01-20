548f37e022f18a2b0d257d5672f5c0c986d84d77
Anteneh M <amoges1@gmail.com>
2019-01-19 20:55:30 -0500

    updated/converted into functional component

--- src/components/Friends.js
+++ src/components/Friends.js
@@ -3,64 +3,54 @@ import Friendres from './Friendres';
 import base from '../base';
 class Friends extends Component {
 
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
             amount: 0,
-            fresload: <p> Loading ...</p>
+            fresload: null
         }
     }
-    componentDidMount() {
-        let fresload;
-        const friend = this.props.frens;
 
+    componentDidMount() {
         //get Friends' restaurants
-        base.fetch(`users/${friend.key}/restaurants`, {
+        base.fetch(`users/${this.props.frens.key}/restaurants`, {
             context: this,
             then(frestaurants){
                 if(frestaurants) {
-                    fresload = <table className="table table-striped "><tbody> {Object.keys(frestaurants).map(key => 
-                        <Friendres user={this.props.user} friendKey={friend.key} user_id={this.props.user_id} key={key} index={key} res={frestaurants[key]}/>
-                    )}</tbody></table>;
-                    this.setState({ amount: Object.keys(frestaurants).length, fresload: fresload})
-                    
-                } else {
-                    this.setState({ fresload: <p> {friend.key} does not have any restaurants </p>})
-        
-                }
-
+                    let fresload = Object.keys(frestaurants).map(key => 
+                        <Friendres _this={this} user={this.props.user} friendKey={this.props.frens.key} user_id={this.props.user_id} key={key} index={key} res={frestaurants[key]}/> );
+                    this.setState({ amount: Object.keys(frestaurants).length, fresload })
+                } 
             }
         });
     }
+    
     render() {
-   
-        const friend = this.props.frens;
-
-
-    return (
-        <div >
-            <div className="container" style={{marginBottom: "10px"}}  role="tablist" aria-multiselectable="true" data-aos="fade-up"  data-aos-duration="1000">
-                <div className="card">
-                    <div className="card-header" style={{backgroundColor: "red"}} id="userheading" data-toggle="collapse" data-parent="useraccordion"
-                    data-target={`#${this.props.index}`} aria-expanded="true" aria-controls="restaurantID">
-                        <h5 className="mb-0 d-flex justify-content-between" style={{color:"white"}}>  {friend.name}           
-                            <span className="badge badge-success"> {this.state.amount}</span>
-                        </h5>
-                    </div> 
-
-                <div id={`${this.props.index}`} className="collapse show" role="tabpanel" aria-labelledby="userheading">
-                    <div className="card-block container" >
-                        <div className="row" style={{paddingTop: "20px"}}>
-                            <div className="container">
-                                       {this.state.fresload}
-                            </div>
+        return (
+            <div >
+                <div className="container" style={{marginBottom: "10px"}}  role="tablist" aria-multiselectable="true" data-aos="fade-up"  data-aos-duration="1000">
+                    <div className="card">
+                        <div className="card-header" style={{backgroundColor: "red"}} id="userheading" data-toggle="collapse" data-parent="useraccordion"
+                        data-target={`#${this.props.index}`} aria-expanded="true" aria-controls="restaurantID">
+                            <h5 className="mb-0 d-flex justify-content-between" style={{color:"white"}}>  {this.props.frens.name}           
+                                <span className="badge badge-success"> {this.state.amount}</span>
+                            </h5>
+                        </div> 
+                    <div id={`${this.props.index}`} className="collapse show" role="tabpanel" aria-labelledby="userheading">
+                        <div className="card-block container" >
+                            <div className="row" style={{paddingTop: "20px"}}>
+                                <div className="container">
+                                    <table className="table table-striped ">
+                                        <tbody>{this.state.fresload}</tbody>
+                                    </table>
+                                </div>
+                            </div> 
                         </div> 
                     </div> 
                 </div> 
-            </div> 
+            </div>
         </div>
-    </div>
-    );
+        );
     }
 }
 
--- src/components/Recsframe.js
+++ src/components/Recsframe.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Rec from './Rec';
 import base from '../base';
 
