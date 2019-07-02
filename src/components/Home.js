c01bf12e7effee9a09289efa525ae57dd6b37362
Anteneh M <amoges1@gmail.com>
2019-07-01 20:58:07 -0400

    converted Home into functional component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,57 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Recsframe from './Recsframe';
 import Resframe from './Resframe';
 import Share from '../modals/Share';
 import { Redirect } from 'react-router';
 
+const Home = ({user, user_id, name}) => {
 
-class Home extends Component {
-
-  
-  render() {
-   
-   
+    if(!name) {
+      return <Redirect to="/"/>
+    }
+    
     //arrays => array.map; objects => Object.keys(objects).map
-    const restaurants = this.props.user.restaurants;
-    //Object.keys(x.state.user.restaurants).map( key => console.log(x.state.user.restaurants[key]))
     let res = []
     let recs = []
-    //Filter restaurants and recommendations based on added status
+    let friends = [] 
 
-    // const resIDs = this.props.user.restaurants ? Object.keys(this.props.user.restaurants).filter(res => res.added === true) : 0
-    // const res = resIDs ? resIDs.map(id => this.props.user.restaurants[id]) : 0
-    // const recsIDs = this.props.user.restaurants ? Object.keys(this.props.user.restaurants).filter(rec => rec.added === false) : 0
-    // const recs = recsIDs ? recsIDs.map(id => this.props.user.restaurants[id]) : 0
-    for (let key in restaurants) {
-      if(restaurants[key].added === true) {
-        res.push(restaurants[key])
-      } else {
-        recs.push(restaurants[key])
-      }
-    }
-    //Filter friends from friend requests
-    // let friends = this.props.user.friends ? Object.keys(this.props.user.friends).filter(friend => friend.connected === true) : 0
-    let friends = [];
-    let user_friends = this.props.user.friends;
-    for (let key in user_friends) {
-      if(user_friends[key].connected) {
-        friends.push(user_friends[key])
-      }
+    //Separate user restaurants and recommendations
+    for (let key in user.restaurants) {
+      user.restaurants[key].added ? res.push(user.restaurants[key]) : recs.push(user.restaurants[key])
     }
    
-    if(!this.props.name ) {
-       return <Redirect to="/"/>
-    } else {
-        return (
-          <div>
-            <Recsframe recs={recs} user={this.props.user} user_id={this.props.user_id}/>
-            <Resframe res={res} user={this.props.user} name={this.props.name}/>
-            <Share friends={friends} name={this.props.name}/>
-          </div>
-        );
-   }
-  }
-  
+    //Filter friends from friend requests
+    for (let key in user.friends) {
+      if(user.friends[key].connected) friends.push(user.friends[key])
+    }
+   
+    return (
+      <div>
+        <Recsframe recs={recs} user={user} user_id={user_id}/>
+        <Resframe res={res} user={user} name={name}/>
+        <Share friends={friends} name={name} _this={this}/>
+      </div>
+    );
 }
 
 export default Home;
